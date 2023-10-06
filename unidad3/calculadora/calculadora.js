389848ce7efcad8fa0902039e39137697e773fcb
ingvega <isc.p.vega@gmail.com>
2023-10-06 13:32:09 -0600

    Validación complementaria con JS

--- unidad3/calculadora/calculadora.js
+++ unidad3/calculadora/calculadora.js
@@ -7,8 +7,21 @@ document.addEventListener('DOMContentLoaded',function(){
             form.classList.add("revisado");
             let num1= parseInt(document.getElementById("txtNum1").value);
             let num2= parseInt(document.getElementById("txtNum2").value);
+            /*Preguntar de uno a uno si alguno de los campos no es válido */
+            //if(isNaN(num1) || isNaN(num2)){
+            /*Preguntarle al form si todo está bien o no */
+            if(!form.checkValidity()){
+                alert('Ingresa los datos');
+                return;
+            }
             let operacion=document.getElementById("cboOperacion").value;
-            debugger;
+            if(operacion=='/' && num2==0){
+                document.getElementById("txtNum2").setCustomValidity('No se acepta cero como divisor de una división');
+                //e.preventDefault();
+                //alert('División por cero');
+                return;
+            }
+            
             let expresion=num1+operacion+num2;
             let resultado=eval(expresion);
             /*if(operacion=='+'){
@@ -22,5 +35,11 @@ document.addEventListener('DOMContentLoaded',function(){
             }*/
             
             alert(resultado);
+    });
+
+    document.querySelector('button[type=reset]').addEventListener('click',function(e){
+        let form=document.querySelector("form");
+        form.classList.remove("revisado");
+        
     });
 });
