7f897d98d77488eb3af7fc972fd528ec259497fd
ingvega <isc.p.vega@gmail.com>
2023-11-15 11:25:20 -0600

    Reestructuración

---
+++ unidad4/ejercicioForm/modelos/usuario.php
@@ -0,0 +1,13 @@
+<?php
+    class Usuario{
+        public $id;
+        public $nombre;
+        public $apellido1;
+        public $apellido2;
+        public $edad;
+        public $email;
+        public $genero;
+        public $intereses;
+        public $password;
+    }
+?>
